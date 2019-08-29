7ad881c6fb8a25da74828df3a347c175a7490f20
fernando <rluis4490@gmail.com>
2019-08-28 18:10:53 -0600

    add node express and hot load

--- middleware/firebase/firebase.utils.js
+++ middleware/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firebase-firestore';
+import '/firebase/auth';
+import '/firebase/firebase-firestore';
 import {getCollectionById,getUidsCollectionById} from './queries';
 
 import * as Consults from './consult'
